8dd5362cdedc99cd2a8a865b61e6bd286b8e7458
Dauphine Burns <dauphineburns@gmail.com>
2021-10-17 14:07:47 -0400

    contact files added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Home from "./Pages/Home/home";
+import Contact from "./Pages/Contact/ contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Route exact path="/" component={Home} />
+      <Route exact path="/contact" component={Contact} />
     </Router>
   );
 }
---
+++ src/Pages/Contact/ contact.js
@@ -0,0 +1,18 @@
+import { React } from "react";
+import "./contact.css";
+
+
+
+function Contact() {
+
+    return (
+
+        <div>
+
+        
+
+        </div>
+        );
+    }
+    
+    export default Contact;
